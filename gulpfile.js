ba3f5e6518423270f394f6b9023b292d9e884b5b
あろえりーな <kiss_mint27@yahoo.co.jp>
2016-08-18 17:20:27 +0900

    add watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 
-gulp.task('less', function () {
-  gulp.src('./less/*.less')
+gulp.task('less', function() {
+  gulp.src('./less/**/*.less')
     .pipe(less())
     .pipe(gulp.dest('./css'));
+});
+
+gulp.task('auto', function() {
+  gulp.watch('./less/*.less', ['less']);
 });
